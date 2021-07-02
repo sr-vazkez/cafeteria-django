ef05d83332a49cc78db4e69230054b3710aab3a6
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-02 13:33:17 -0500

    modificando modelo

--- cafeteria/pages/models.py
+++ cafeteria/pages/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Page(models.Model):
     title = models.CharField(verbose_name="Titulo", max_length=200)
     content = models.TextField(verbose_name="Contenido")
+    order = models.SmallIntegerField(verbose_name="Orden", default=0)
     created = models.DateField(
         auto_now_add=True, verbose_name="Fecha de creacion")
     updated = models.DateField(auto_now=True, verbose_name="Fecha de Edicion")
@@ -13,7 +14,7 @@ class Page(models.Model):
     class Meta:
         verbose_name = "pagina"
         verbose_name_plural = "paginas"
-        ordering = ['title']
+        ordering = ['order', 'title']
 
     def __str__(self):
         return self.title
