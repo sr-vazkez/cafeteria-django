0cd5672db321ceda2e3aca7328062e6cda413f76
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-01 20:29:20 -0500

    iniciando rutas

--- cafeteria/core/views.py
+++ cafeteria/core/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse("Inicio")
+
+
+def about(request):
+    return HttpResponse("historia")
+
+
+def services(request):
+    return HttpResponse("servicios")
+
+
+def store(request):
+    return HttpResponse("visitanos")
+
+
+def contact(request):
+    return HttpResponse("contacto")
+
+
+def blog(request):
+    return HttpResponse("blog")
+
+
+def sample(request):
+    return HttpResponse("sample")
