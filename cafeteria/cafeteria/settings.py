83d3e31016dec85c0f889bb000c2aa3c40d2150c
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-02 14:51:58 -0500

    commit de prueba

--- cafeteria/cafeteria/settings.py
+++ cafeteria/cafeteria/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'ckeditor',
     'core',
     'pages.apps.PagesConfig',
     'services.apps.ServicesConfig',
