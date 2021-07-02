f709e81b527a82649b08672ff42105b508925974
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-01 23:59:15 -0500

    generando url dinamica

--- cafeteria/blog/urls.py
+++ cafeteria/blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     # Paths del Core
     path('', views.blog, name="blog"),
+    path('category/<int:category_id>/', views.category, name="category"),
 
 ]
--- cafeteria/blog/views.py
+++ cafeteria/blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
 # Create your views here.
 
 
 def blog(request):
     posts = Post.objects.all()
     return render(request, "blog/blog.html", {'posts': posts})
+
+
+def category(request, category_id):
+    category = Category.objects.get(id=category_id)
+    return render(request, "blog/category.html", {'category': category})
