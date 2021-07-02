417e3709d1a48619e6ed04b8b9fba15a62aa2c96
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-01 22:54:33 -0500

    creando modelos del blog

--- cafeteria/blog/models.py
+++ cafeteria/blog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Nombre")
+    created = models.DateField(
+        auto_now_add=True, verbose_name="Fecha de creacion")
+    updated = models.DateField(auto_now=True, verbose_name="Fecha de Edicion")
+
+    class Meta:
+        verbose_name = "categoria"
+        verbose_name_plural = "categorias"
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Titulo")
+    content = models.TextField(verbose_name="Contenido")
+    published = models.DateTimeField(
+        verbose_name="Fecha de Publicacion", default=timezone.now())
+    image = models.ImageField(verbose_name="Image",
+                              upload_to="blog", null=True, blank=True)
+    author = models.ForeignKey(
+        User, verbose_name="Autor", on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category, verbose_name="Categorias")
+    created = models.DateField(
+        auto_now_add=True, verbose_name="Fecha de creacion")
+    updated = models.DateField(auto_now=True, verbose_name="Fecha de Edicion")
+
+    class Meta:
+        verbose_name = "entrada"
+        verbose_name_plural = "entradas"
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
