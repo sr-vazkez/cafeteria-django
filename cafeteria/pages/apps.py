654224c8cd2447948a4e6f00f63fde85829c6eab
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-02 12:51:08 -0500

    haciendo migraciones

--- cafeteria/pages/apps.py
+++ cafeteria/pages/apps.py
@@ -2,6 +2,5 @@ from django.apps import AppConfig
 
 
 class PagesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'pages'
     verbose_name = 'Gestor de paginas'
