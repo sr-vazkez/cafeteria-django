561614f64fbf1c12d0b29de957831acdf1586330
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-01 20:36:04 -0500

    rutas listas

--- cafeteria/cafeteria/urls.py
+++ cafeteria/cafeteria/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
+    # Paths del Core
+    path('', views.home, name="home"),
+    path('about/', views.home, name="about"),
+    path('services/', views.home, name="services"),
+    path('store/', views.home, name="store"),
+    path('contact/', views.home, name="contact"),
+    path('blog/', views.home, name="blog"),
+    path('sample/', views.home, name="sample"),
+
+    # Rutas del admin
     path('admin/', admin.site.urls),
 ]
