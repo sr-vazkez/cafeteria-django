c0093da1ecffcde5e899d7ec55e09776ebb196f0
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-02 15:28:36 -0500

    haciendo formulario

--- cafeteria/contact/views.py
+++ cafeteria/contact/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from .forms import ContactForm
 # Create your views here.
 
 
 def contact(request):
-    return render(request, "contact/contact.html")
+    contact_form = ContactForm()
+    return render(request, "contact/contact.html", {'form': contact_form})
